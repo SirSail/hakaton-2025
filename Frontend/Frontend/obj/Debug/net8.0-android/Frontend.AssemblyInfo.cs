09937a8c5b2c5717b6c45c373581747eed37ac44
SirSail <zeglinski.jakub@gmail.com>
2025-04-18 19:22:58 +0200

    Design kalendarz

--- Frontend/Frontend/obj/Debug/net8.0-android/Frontend.AssemblyInfo.cs
+++ Frontend/Frontend/obj/Debug/net8.0-android/Frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0833d2308cc42cca2f886c8e26d30a3bad7a0204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1c60b601ffd063d727093270031a8c180bdb9fa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
