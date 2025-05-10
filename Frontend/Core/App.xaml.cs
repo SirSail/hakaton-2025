504dfe5e4d43eb7e3bb4371c6da5cee4f1c43e48
IZdaniewicz <igor.zdaniewicz@o2.pl>
2025-05-10 13:57:59 +0200

    poprawka

--- Frontend/Core/App.xaml.cs
+++ Frontend/Core/App.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using Core.Authorize.Services;
 using Core.PushNotifications.Services;
-using System.Diagnostics;
-using System.Runtime.CompilerServices;
 
 namespace Core
 {
@@ -17,37 +15,4 @@ namespace Core
 
         }
     }
-//        public App(PushNotificationsService notificationService)
-//        {
-//            InitializeComponent();
-//            MainPage = new MainPage();
-
-//            _pushNotificationService = notificationService;
-//#if ANDROID
-
-//            Plugin.Firebase.CloudMessaging.CrossFirebaseCloudMessaging.Current.NotificationReceived += (s, message) =>
-//            {
-//                var title = message.Notification?.Title ?? "Brak tytułu";
-//                var body = message.Notification?.Body ?? string.Empty;
-
-//                string redirectRoute = string.Empty;
-//                message.Notification.Data.TryGetValue("redirect_route", out redirectRoute);
-
-//#if DEBUG
-//                Debug.WriteLine($"Otrzymano powiadomiienie push: {title}: {body} (redirectUrl : {redirectRoute})");
-//#endif
-
-//                _pushNotificationService.RaiseNotification(new PushNotification { Title = title, Body = body, RedirectRoute = redirectRoute });
-
-//                Core.Platforms.Android.Services.PushNotificationService.ShowNotification(title, body, redirectRoute);
-//            };
-
-//            Plugin.Firebase.CloudMessaging.CrossFirebaseCloudMessaging.Current.TokenChanged += async (s, token) =>
-//            {
-//                //await _authService.RegisterFCMToken();
-//                Debug.WriteLine($"FCM Token: {token}");
-//            };
-//#endif
-//        }
-//    }
 }
