45ee0bef585faf7014b9e9615e655569e207f863
IZdaniewicz <igor.zdaniewicz@o2.pl>
2025-05-13 20:57:44 +0200

    wersja z nowa mapka

--- Frontend/Core/App.xaml.cs
+++ Frontend/Core/App.xaml.cs
@@ -5,12 +5,35 @@ namespace Core
 {
     public partial class App : Application
     {
+        private readonly PushNotificationsService _pushNotificationService;
 
-        public App()
+        public App(PushNotificationsService notificationService)
         {
             InitializeComponent();
             MainPage = new MainPage();
 
+            _pushNotificationService = notificationService;
+#if ANDROID
+
+            Plugin.Firebase.CloudMessaging.CrossFirebaseCloudMessaging.Current.NotificationReceived += (s, message) =>
+            {
+                var title = message.Notification?.Title ?? "Brak tytułu";
+                var body = message.Notification?.Body ?? string.Empty;
+
+                string redirectRoute = string.Empty;
+                message.Notification.Data.TryGetValue("redirect_route", out redirectRoute);
+
+                _pushNotificationService.RaiseNotification(new PushNotification { Title = title, Body = body, RedirectRoute = redirectRoute });
+
+                Core.Platforms.Android.Services.PushNotificationService.ShowNotification(title, body, redirectRoute);
+            };
+
+            //Plugin.Firebase.CloudMessaging.CrossFirebaseCloudMessaging.Current.TokenChanged += async (s, token) =>
+            //{
+            //    await _authService.RegisterFCMToken();
+            //};
+#endif
         }
     }
 }
+
